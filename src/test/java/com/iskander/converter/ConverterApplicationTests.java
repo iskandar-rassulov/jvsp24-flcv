7b668826f152377fc16dd4557ba34bef04c3c5e6
iskander-rassulov <iskander.rassulov.2002@gmail.com>
2024-12-14 17:17:54 +0500

    init commit

---
+++ src/test/java/com/iskander/converter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iskander.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
